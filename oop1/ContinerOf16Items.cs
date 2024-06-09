b3a8a1d8000a50de878db8bd9ceddf11a9f176ff
yair vilner <yair.vilner@gmail.com>
2024-06-09 23:21:28 +0300

    9/6/2024 - home

--- oop1/ContinerOf16Items.cs
+++ oop1/ContinerOf16Items.cs
@@ -2,217 +2,238 @@
 
 namespace oop1
 {
-    public partial class MyColletion<T>
+    [Serializable]
+    internal class ContinerOf16Items<T>
     {
-        [Serializable]
-        protected internal class ContinerOf16Items<T1>
+        public ContinerOf16Items<ContinerOf16Items<T>> father;
+        public T item0;
+        public T item1;
+        public T item2;
+        public T item3;
+        public T item4;
+        public T item5;
+        public T item6;
+        public T item7;
+        public T item8;
+        public T item9;
+        public T item10;
+        public T item11;
+        public T item12;
+        public T item13;
+        public T item14;
+        public T item15;
+
+        public ContinerOf16Items() { }
+        public ContinerOf16Items(ContinerOf16Items<ContinerOf16Items<T>> father) =>
+            this.father = father;
+        public ContinerOf16Items(T item0, T item1, T item2, T item3, T item4, T item5, T item6, T item7,
+                                 T item8, T item9, T item10, T item11, T item12, T item13, T item14, T item15,
+                                 ContinerOf16Items<ContinerOf16Items<T>> father = null)
         {
-            public ContinerOf16Items<ContinerOf16Items<T1>> father;
-            public T1 item0;
-            public T1 item1;
-            public T1 item2;
-            public T1 item3;
-            public T1 item4;
-            public T1 item5;
-            public T1 item6;
-            public T1 item7;
-            public T1 item8;
-            public T1 item9;
-            public T1 item10;
-            public T1 item11;
-            public T1 item12;
-            public T1 item13;
-            public T1 item14;
-            public T1 item15;
+            this.item0 = item0;
+            this.item1 = item1;
+            this.item2 = item2;
+            this.item3 = item3;
+            this.item4 = item4;
+            this.item5 = item5;
+            this.item6 = item6;
+            this.item7 = item7;
+            this.item8 = item8;
+            this.item9 = item9;
+            this.item10 = item10;
+            this.item11 = item11;
+            this.item12 = item12;
+            this.item13 = item13;
+            this.item14 = item14;
+            this.item15 = item15;
+            this.father = father;
+        }
 
-            public ContinerOf16Items() { }
-            public ContinerOf16Items(ContinerOf16Items<ContinerOf16Items<T1>> father) =>
-                this.father = father;
-            public ContinerOf16Items(T1 item0, T1 item1, T1 item2, T1 item3, T1 item4, T1 item5, T1 item6, T1 item7,
-                                     T1 item8, T1 item9, T1 item10, T1 item11, T1 item12, T1 item13, T1 item14, T1 item15,
-                                     ContinerOf16Items<ContinerOf16Items<T1>> father = null)
+        public T this[int index]
+        {
+            get
             {
-                this.item0 = item0;
-                this.item1 = item1;
-                this.item2 = item2;
-                this.item3 = item3;
-                this.item4 = item4;
-                this.item5 = item5;
-                this.item6 = item6;
-                this.item7 = item7;
-                this.item8 = item8;
-                this.item9 = item9;
-                this.item10 = item10;
-                this.item11 = item11;
-                this.item12 = item12;
-                this.item13 = item13;
-                this.item14 = item14;
-                this.item15 = item15;
-                this.father = father;
-            }
-
-            public T1 this[int index]
-            {
-                get
+                if (index < 0 || index >= 16)
+                    throw new IndexOutOfRangeException("Index must be between 0 and 15");
+                // get the item using a 4 staged binary search
+                int level1 = index % 2;
+                index /= 2;
+                int level2 = index % 2;
+                index /= 2;
+                int level3 = index % 2;
+                index /= 2;
+                int level4 = index % 2;
+                if (level4 == 0)
                 {
-                    if (index < 0 || index >= 16)
-                        throw new IndexOutOfRangeException("Index must be between 0 and 15");
-                    // get the item using a 4 staged binary search
-                    if (index / 16 == 0)
+                    if (level3 == 0)
                     {
-                        if (index / 8 == 0)
+                        if (level2 == 0)
                         {
-                            if (index / 4 == 0)
-                            {
-                                if (index / 2 == 0) return item0;
-                                else return item1;
-                            }
-                            else
-                            {
-                                if (index / 2 == 0) return item2;
-                                else return item3;
-                            }
+                            if (level1 == 0) return item0;
+                            else return item1;
                         }
                         else
                         {
-                            if (index / 4 == 0)
-                            {
-                                if (index / 2 == 0) return item4;
-                                else return item5;
-                            }
-                            else
-                            {
-                                if (index / 2 == 0) return item6;
-                                else return item7;
-                            }
+                            if (level1 == 0) return item2;
+                            else return item3;
                         }
                     }
                     else
                     {
-                        if (index / 8 == 0)
+                        if (level2 == 0)
                         {
-                            if (index / 4 == 0)
-                            {
-                                if (index / 2 == 0) return item8;
-                                else return item9;
-                            }
-                            else
-                            {
-                                if (index / 2 == 0) return item10;
-                                else return item11;
-                            }
+                            if (level1 == 0) return item4;
+                            else return item5;
                         }
                         else
                         {
-                            if (index / 4 == 0)
-                            {
-                                if (index / 2 == 0) return item12;
-                                else return item13;
-                            }
-                            else
-                            {
-                                if (index / 2 == 0) return item14;
-                                else return item15;
-                            }
+                            if (level1 == 0) return item6;
+                            else return item7;
                         }
                     }
                 }
-                set
+                else
                 {
-                    if (index < 0 || index >= 16)
-                        throw new IndexOutOfRangeException("Index must be between 0 and 15");
-                    // access the item using a 4 staged binary search
-                    if (index / 16 == 0)
+                    if (level3 == 0)
                     {
-                        if (index / 8 == 0)
+                        if (level2 == 0)
                         {
-                            if (index / 4 == 0)
-                            {
-                                if (index / 2 == 0) item0 = value;
-                                else item1 = value;
-                            }
-                            else
-                            {
-                                if (index / 2 == 0) item2 = value;
-                                else item3 = value;
-                            }
+                            if (level1 == 0) return item8;
+                            else return item9;
                         }
                         else
                         {
-                            if (index / 4 == 0)
-                            {
-                                if (index / 2 == 0) item4 = value;
-                                else item5 = value;
-                            }
-                            else
-                            {
-                                if (index / 2 == 0) item6 = value;
-                                else item7 = value;
-                            }
+                            if (level1 == 0) return item10;
+                            else return item11;
                         }
                     }
                     else
                     {
-                        if (index / 8 == 0)
+                        if (level2 == 0)
                         {
-                            if (index / 4 == 0)
-                            {
-                                if (index / 2 == 0) item8 = value;
-                                else item9 = value;
-                            }
-                            else
-                            {
-                                if (index / 2 == 0) item10 = value;
-                                else item11 = value;
-                            }
+                            if (level1 == 0) return item12;
+                            else return item13;
                         }
                         else
                         {
-                            if (index / 4 == 0)
-                            {
-                                if (index / 2 == 0) item12 = value;
-                                else item13 = value;
-                            }
-                            else
-                            {
-                                if (index / 2 == 0) item14 = value;
-                                else item15 = value;
-                            }
+                            if (level1 == 0) return item14;
+                            else return item15;
                         }
                     }
                 }
             }
+            set
+            {
+                if (index < 0 || index >= 16)
+                    throw new IndexOutOfRangeException("Index must be between 0 and 15");
+                // access the item using a 4 staged binary search
+                int level1 = index % 2;
+                index /= 2;
+                int level2 = index % 2;
+                index /= 2;
+                int level3 = index % 2;
+                index /= 2;
+                int level4 = index % 2;
+                if (level4 == 0)
+                {
+                    if (level3 == 0)
+                    {
+                        if (level2 == 0)
+                        {
+                            if (level1 == 0) item0 = value;
+                            else item1 = value;
+                        }
+                        else
+                        {
+                            if (level1 == 0) item2 = value;
+                            else item3 = value;
+                        }
+                    }
+                    else
+                    {
+                        if (level2 == 0)
+                        {
+                            if (level1 == 0) item4 = value;
+                            else item5 = value;
+                        }
+                        else
+                        {
+                            if (level1 == 0) item6 = value;
+                            else item7 = value;
+                        }
+                    }
+                }
+                else
+                {
+                    if (level3 == 0)
+                    {
+                        if (level2 == 0)
+                        {
+                            if (level1 == 0) item8 = value;
+                            else item9 = value;
+                        }
+                        else
+                        {
+                            if (level1 == 0) item10 = value;
+                            else item11 = value;
+                        }
+                    }
+                    else
+                    {
+                        if (level2 == 0)
+                        {
+                            if (level1 == 0) item12 = value;
+                            else item13 = value;
+                        }
+                        else
+                        {
+                            if (level1 == 0) item14 = value;
+                            else item15 = value;
+                        }
+                    }
+                }
+            }
+        }
 
 #if DEBUG
-            #region tests
-            internal static bool IsTestsPassed => TestIndexer();
-            internal static bool TestIndexer()
-            {
-                ContinerOf16Items<int> testContiner = new(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
-                for (int i = 0; i < 16; i++)
-                    if (!(testContiner[i] == i))
-                        return false;
-                for (int i = 0; i < 16; i++)
-                    testContiner[i] = 16 - i;
-                return testContiner.item0 == 16 &&
-                       testContiner.item1 == 15 &&
-                       testContiner.item2 == 14 &&
-                       testContiner.item3 == 13 &&
-                       testContiner.item4 == 12 &&
-                       testContiner.item5 == 11 &&
-                       testContiner.item6 == 10 &&
-                       testContiner.item7 == 9 &&
-                       testContiner.item8 == 8 &&
-                       testContiner.item9 == 7 &&
-                       testContiner.item10 == 6 &&
-                       testContiner.item11 == 5 &&
-                       testContiner.item12 == 4 &&
-                       testContiner.item13 == 3 &&
-                       testContiner.item14 == 2 &&
-                       testContiner.item15 == 1;
-            } 
-            #endregion
+        #region tests
+        internal static bool IsTestsPassed => TestIndexer();
+        internal static bool TestIndexer()
+        {
+            ContinerOf16Items<int> testContiner = new(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+            for (int i = 0; i < 16; i++)
+                if (!(testContiner[i] == i))
+                    return false;
+            for (int i = 0; i < 16; i++)
+                testContiner[i] = 16 - i;
+            return testContiner.item0 == 16 &&
+                   testContiner.item1 == 15 &&
+                   testContiner.item2 == 14 &&
+                   testContiner.item3 == 13 &&
+                   testContiner.item4 == 12 &&
+                   testContiner.item5 == 11 &&
+                   testContiner.item6 == 10 &&
+                   testContiner.item7 == 9 &&
+                   testContiner.item8 == 8 &&
+                   testContiner.item9 == 7 &&
+                   testContiner.item10 == 6 &&
+                   testContiner.item11 == 5 &&
+                   testContiner.item12 == 4 &&
+                   testContiner.item13 == 3 &&
+                   testContiner.item14 == 2 &&
+                   testContiner.item15 == 1;
+        }
+        #endregion
 #endif
+    }
+    internal static class ContinerOf16ItemsExtentions
+    {
+        public static ContinerOf16Items<T> GetNextLevelContiner<T>(this ContinerOf16Items<ContinerOf16Items<T>> continer, int index)
+        {
+            if (continer[index] == null)
+                continer[index] = new ContinerOf16Items<T>(continer);
+            return continer[index];
         }
     }
+
 }
--- oop1/MyColletion.cs
+++ oop1/MyColletion.cs
@@ -52,7 +52,10 @@ namespace oop1
                 int level7 = index % 16;
                 index /= 16;
                 int level8 = index % 16;
-                internalStorage[level8][level7][level6][level5][level4][level3][level2][level1] = value;
+                lock (internalStorage)
+                {
+                    internalStorage[level8][level7][level6][level5][level4][level3][level2][level1] = value;
+                }
             }
         }
         object IList.this[int index]
@@ -71,7 +74,7 @@ namespace oop1
         {
             if (Count + 1 == int.MaxValue)
                 throw new InvalidOperationException("Collection length out of bounds");
-            int itemCount = Count++;
+            int itemCount = Count;
             int level1 = itemCount % 16;
             itemCount /= 16;
             int level2 = itemCount % 16;
@@ -87,7 +90,10 @@ namespace oop1
             int level7 = itemCount % 16;
             itemCount /= 16;
             int level8 = itemCount % 16;
-            internalStorage[level8][level7][level6][level5][level4][level3][level2][level1] = item;
+            internalStorage.GetNextLevelContiner(level8).GetNextLevelContiner(level7).GetNextLevelContiner(level6)
+                           .GetNextLevelContiner(level5).GetNextLevelContiner(level4).GetNextLevelContiner(level3)
+                           .GetNextLevelContiner(level2)[level1] = item;
+            Count++;
         }
         int IList.Add(object value)
         {
@@ -108,9 +114,12 @@ namespace oop1
         }
         public void CopyTo(T[] array, int arrayIndex)
         {
-            if (array == null) throw new ArgumentNullException(nameof(array), "array cannot be null");
-            if (arrayIndex < 0) throw new ArgumentOutOfRangeException(nameof(arrayIndex), arrayIndex, "arrayIndex cannot be negative");
-            if (array.Length - arrayIndex < Count) throw new ArgumentException("array index out of range", nameof(arrayIndex));
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), "array cannot be null");
+            if (arrayIndex < 0)
+                throw new ArgumentOutOfRangeException(nameof(arrayIndex), arrayIndex, "arrayIndex cannot be negative");
+            if (array.Length - arrayIndex < Count)
+                throw new ArgumentException("array index out of range", nameof(arrayIndex));
             for (int i = 0; i < Count; i++)
                 array[arrayIndex++] = this[i];
         }
@@ -142,10 +151,115 @@ namespace oop1
             value is T testedValue ? IndexOf(testedValue) : -1;
         public void Insert(int index, T item)
         {
+            int fullIndex = index++;
+            int level1 = index % 16;
+            index /= 16;
+            int level2 = index % 16;
+            index /= 16;
+            int level3 = index % 16;
+            index /= 16;
+            int level4 = index % 16;
+            index /= 16;
+            int level5 = index % 16;
+            index /= 16;
+            int level6 = index % 16;
+            index /= 16;
+            int level7 = index % 16;
+            index /= 16;
+            int level8 = index % 16;
+            index = fullIndex;
+
+            int correntCount = Count + 1;
+            int level1Max = correntCount % 16;
+            correntCount /= 16;
+            int level2Max = correntCount % 16;
+            correntCount /= 16;
+            int level3Max = correntCount % 16;
+            correntCount /= 16;
+            int level4Max = correntCount % 16;
+            correntCount /= 16;
+            int level5Max = correntCount % 16;
+            correntCount /= 16;
+            int level6Max = correntCount % 16;
+            correntCount /= 16;
+            int level7Max = correntCount % 16;
+            correntCount /= 16;
+            int level8Max = correntCount % 16;
+
             Add(default);
-            Count++;
-            for (int i = Count - 1; i > index; i--)
-                this[i] = this[i - 1];
+            T lastValue = this[index], newValue;
+            ContinerOf16Items<T> level1Continer = internalStorage[level8][level7][level6][level5][level4][level3][level2];
+            bool isLastRoundDone = false;
+            lock (internalStorage)
+            {
+                while (level8 <= 16)                                //10,000,000
+                {
+                    while (level7 < 16)                             //1,000,000
+                    {
+                        while (level6 < 16)                         //100,000
+                        {
+                            while (level5 < 16)                     //10,000
+                            {
+                                while (level4 < 16)                 //1,000
+                                {
+                                    while (level3 < 16)             //100
+                                    {
+                                        while (level2 < 16)         //10
+                                        {
+                                            while (level1 < 16)     //1
+                                            {
+                                                newValue = level1Continer[level1];
+                                                level1Continer[level1] = lastValue;
+                                                lastValue = newValue;
+                                                if (level8 == level8Max && level7 == level7Max && level6 == level6Max && level5 == level5Max && level4 == level4Max && level3 == level3Max && level2 == level2Max && level1 == level1Max)
+                                                {
+                                                    isLastRoundDone = true;
+                                                    break;
+                                                }
+                                            }
+                                            if (isLastRoundDone)
+                                                break;
+                                            level1Continer = level1Continer.father[level2];
+                                            level1 = 0;
+                                            level2++;
+                                        }
+                                        if (isLastRoundDone)
+                                            break;
+                                        level1Continer = level1Continer.father.father[level3][0];
+                                        level2 = 0;
+                                        level3++;
+                                    }
+                                    if (isLastRoundDone)
+                                        break;
+                                    level1Continer = level1Continer.father.father.father[level4][0][0];
+                                    level3 = 0;
+                                    level4++;
+                                }
+                                if (isLastRoundDone)
+                                    break;
+                                level1Continer = level1Continer.father.father.father.father[level5][0][0][0];
+                                level4 = 0;
+                                level5++;
+                            }
+                            if (isLastRoundDone)
+                                break;
+                            level1Continer = level1Continer.father.father.father.father.father[level6][0][0][0][0];
+                            level5 = 0;
+                            level6++;
+                        }
+                        if (isLastRoundDone)
+                            break;
+                        level1Continer = level1Continer.father.father.father.father.father.father[level7][0][0][0][0][0];
+                        level6 = 0;
+                        level7++;
+                    }
+                    if (isLastRoundDone)
+                        break;
+                    level1Continer = level1Continer.father.father.father.father.father.father.father[level8][0][0][0][0][0][0];
+                    level7 = 0;
+                    level8++;
+                }
+            }
             this[index] = item;
         }
         void IList.Insert(int index, object value)
@@ -171,6 +285,8 @@ namespace oop1
             for (int i = index; i < Count - 1; i++)
                 this[i] = this[i + 1];
             Count--;
-        }
+        }           //TODO: Add a lock() to RemoveAt()
+        public override string ToString() =>
+            string.Format("{0}, count:{1}", base.ToString().Split('`')[0], Count.ToString());
     }
 }
